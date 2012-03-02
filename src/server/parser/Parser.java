721daeb7a35729d6cc8977af2d280296cbd1a257
user <user@MPC>
2012-03-02 19:30:42 +0100

    Very Basic implementation

--- src/server/parser/Parser.java
+++ src/server/parser/Parser.java
@@ -2,5 +2,16 @@ package server.parser;
 
 public class Parser 
 {
+	
+	/**
+	 * Parses the given Message and sends the corresponding answers
+	 * @param s_MSG the Message the Server socket received
+	 * @return What the Socket should answer
+	 */
+	public String Parse(String s_MSG)
+	{
+		
+		return "";
+	}
 
 }

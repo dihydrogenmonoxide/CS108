67a25beb90b1d8c1779d89bf5579c63f5752f7e6
fox918 <oliver.wisler@gmail.com>
2012-03-03 01:16:41 +0100

    prepared everything for connecting to server. Username validation.

--- src/client/lobby/ClientLobby.java
+++ src/client/lobby/ClientLobby.java
@@ -1,8 +1,14 @@
 package client.lobby;
 
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+
+import shared.Log;
+
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 
 /** this is the lobby at the start.
@@ -32,20 +38,45 @@ public class ClientLobby extends JFrame{
 
 
 		/*
-		 * Set up lobby
+		 * Set up background
+		 * The bg-image is drawn in a JPanel which is laid under all the other panes with User-IO
+		 * */
+		JPanel bg = new JPanel(){
+			/**paint the swiss map in the background*/
+			public void paintComponent(Graphics g)
+			{
+				BufferedImage img = null;
+				try 
+				{
+					img = ImageIO.read(new File("lobby_bg.jpg"));
+					g.drawImage(img, 0, 0, i_LobbyX, i_LobbyY, 0, 0, img.getWidth(), img.getHeight(), new Color(0,0,0), null);
+				} 
+				catch (Exception e) 
+				{
+					e.printStackTrace();
+				}
+			}
+		};
+		
+		/*
+		 * Set up lobby / inputs
 		 * */
 		lobby = new JFrame("SwissDefcon Lobby");
 		lobby.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		lobby.setSize(i_LobbyX,i_LobbyY);
+		lobby.setResizable(false);
 		lobby.setLocation(screenX/2-i_LobbyX/2,screenY/2-i_LobbyX/2);
-
+		lobby.setContentPane(bg);
+		
 		SelectServer s=new SelectServer();
-		s.setScreen(i_LobbyX,i_LobbyY);
-		lobby.setContentPane(s);
+		lobby.add(s);
+		Log.InformationLog("you can now select a server");
+		
+		
 		lobby.setVisible(true);
-
-
 	}
 
 
 }
+
+
--- src/client/lobby/SelectServer.java
+++ src/client/lobby/SelectServer.java
@@ -2,17 +2,16 @@ package client.lobby;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-import java.awt.image.BufferedImage;
-import java.io.File;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.Vector;
 
-import javax.imageio.ImageIO;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFormattedTextField;
@@ -21,21 +20,26 @@ import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ListSelectionModel;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
+import javax.swing.event.*;
 
 import shared.Log;
 import shared.ServerAddress;
 import client.net.DiscoveryClient;
-
+/**
+ * This creates a dialog to join a server.
+ * 
+ * @param none
+ * @return the Server which to Connect to.
+ * */
 public class SelectServer extends JPanel{
+
 	private static final long serialVersionUID = 1L;
-	private Vector<ServerAddress> vs_Servers;
+	private Vector<ServerAddress> vs_Servers;	//holds the found servers
 	private JList jl_Dialog;
 	private JButton bt_Join;
-	private String[] sa_NoServers={"suchen ...","bitte haben Sie Geduld"};
-	private int i_ScreenX;
-	private int i_ScreenY;
+	private JLabel lbl_Error;
+	private JFormattedTextField jft_Username;
+	private String[] sa_NoServers={"suchen ...","bitte haben Sie Geduld"}; //printed when no servers active
 
 	public SelectServer()
 	{
@@ -43,7 +47,7 @@ public class SelectServer extends JPanel{
 
 		this.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
-		
+
 		JLabel lbl_Dialog = new JLabel();
 		lbl_Dialog.setText("Wählen Sie ihren Server:");
 		lbl_Dialog.setBackground(new Color (255,255,255));
@@ -54,22 +58,25 @@ public class SelectServer extends JPanel{
 		c.gridx = 0;
 		c.gridy = 0;
 		this.add(lbl_Dialog, c);
-		
+
 		jl_Dialog = new JList(new DefaultListModel());
 		jl_Dialog.setVisibleRowCount(5);
 		jl_Dialog.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		jl_Dialog.setLayoutOrientation(JList.VERTICAL);
-		
+
 		vs_Servers = new Vector<ServerAddress>();
-		
+
 		jl_Dialog.setListData(sa_NoServers);
 		jl_Dialog.setEnabled(false); //because no server found yet
 
-		//suche regelmässig nach servern
+		/*
+		 *Timer, so we will scan every 6sec for new servers.
+		 *Servers found are copied in vs_Servers and displayed in the SelectList
+		 * */
 		Timer timer = new Timer();
 		int i_Delay = 1000;   
 		int i_Period = 6000;
-		
+
 		timer.scheduleAtFixedRate(new TimerTask() {
 			public void run() {
 				DiscoveryClient s = new DiscoveryClient();
@@ -81,13 +88,13 @@ public class SelectServer extends JPanel{
 					// not important
 					e.printStackTrace();
 				}
-				
+				int i_temp= jl_Dialog.getSelectedIndex();
 				vs_Servers.clear();
 				for(ServerAddress a : s.GetList())
 				{
 					vs_Servers.add(a);
 				}
-				
+
 				if(vs_Servers.isEmpty()) //no server active
 				{
 					jl_Dialog.clearSelection();
@@ -97,20 +104,26 @@ public class SelectServer extends JPanel{
 				}else
 				{	
 					jl_Dialog.setListData(vs_Servers);
+					jl_Dialog.setSelectedIndex(i_temp);
 					jl_Dialog.setEnabled(true);
 				}
 				jl_Dialog.repaint();
 				t.interrupt();
 			}
 		}, i_Delay, i_Period);
-		
+
 		jl_Dialog.addListSelectionListener(new ListSelectionListener() {
-		      public void valueChanged(ListSelectionEvent evt) {
-		          Log.DebugLog("Choosen "+vs_Servers.elementAt(evt.getFirstIndex())+" as Server");
-		          bt_Join.setEnabled(true);
-		        }
+			public void valueChanged(ListSelectionEvent evt) {
+				try{
+					Log.DebugLog("Choosen "+vs_Servers.elementAt(evt.getFirstIndex())+" as Server");
+					bt_Join.setEnabled(true);
+				}catch(ArrayIndexOutOfBoundsException e){
+					Log.DebugLog("Tried to choose an invalid/inactive Server");
+
+				}
+			}
 		});
-		
+
 		JScrollPane jsp_ServerPane = new JScrollPane(jl_Dialog);
 		jsp_ServerPane.setPreferredSize(new Dimension(250, 80));
 		c.fill = GridBagConstraints.HORIZONTAL;
@@ -121,8 +134,8 @@ public class SelectServer extends JPanel{
 		c.gridy = 1;
 		c.insets=new Insets(0,0,10,0);
 		this.add(jsp_ServerPane, c);
-		
-		
+
+
 		JLabel lbl_User = new JLabel();
 		lbl_User.setText("Wählen Sie ihren Benutzernamen:");
 		lbl_User.setBackground(new Color (255,255,255));
@@ -136,10 +149,10 @@ public class SelectServer extends JPanel{
 		c.gridy = 3;
 		c.insets=new Insets(10,5,2,5);
 		this.add(lbl_User, c);
-		
-		JFormattedTextField jft_UserName = new JFormattedTextField();
-		jft_UserName.setColumns(10);
-		jft_UserName.setText(System.getProperty("user.name"));
+
+		jft_Username = new JFormattedTextField();
+		jft_Username.setColumns(10);
+		jft_Username.setText(checkUsername(System.getProperty("user.name")));
 		c.fill = GridBagConstraints.LINE_END;
 		c.ipady = -1;
 		c.weightx = 0.0;
@@ -147,38 +160,97 @@ public class SelectServer extends JPanel{
 		c.gridx = 0;
 		c.gridy = 4;
 		c.insets=new Insets(0,0,10,5);
-		this.add(jft_UserName, c);
-		
-		
+		this.add(jft_Username, c);
+
+
 		bt_Join = new JButton("Server beitreten");
 		bt_Join.setEnabled(false);
+		bt_Join.addActionListener(new ActionListener(){
+			/**Actionlistener for the "join Server" button. 
+			 * validates the Username and set it to default if null.
+			 * then validates Server Selection.
+			 * Establishes a connection to the selected server and then throw event.
+			 * */
+			public void actionPerformed(ActionEvent arg0) {
+				Log.DebugLog("Trying to join Server");
+
+				String s_Username;
+				try
+				{
+					s_Username = checkUsername(jft_Username.getText());
+				}
+				catch(NullPointerException e)
+				{
+					Log.DebugLog("-->no Username given, set to default");
+					s_Username = "fox1337";
+				}
+
+				try
+				{
+					Log.DebugLog("-->choosen "+vs_Servers.elementAt(jl_Dialog.getSelectedIndex())+" as Server");
+					
+					//connect to server
+					try
+					{
+						/*
+						 * 
+						 * 
+						 *   DO STUFF HERE, idee: set flag and  throw event to ClientLobby
+						 * 
+						 * 
+						 * 
+						 * */	
+						Log.InformationLog("-->Connected to **** as "+s_Username);
+					}
+					catch(Exception e)
+					{
+						Log.DebugLog("-->Could not join Server");
+						lbl_Error.setText("konnte nicht mit Server verbinden");
+						lbl_Error.setVisible(true);
+					}
+				}
+				catch(ArrayIndexOutOfBoundsException e)
+				{
+					Log.DebugLog("--Tried to choose an invalid/inactive Server");
+					if(jl_Dialog.getSelectedIndex()<0)
+					{
+						lbl_Error.setText("kein Server ausgewählt");
+						lbl_Error.setVisible(true);
+					}
+				}
+			}
+		});
+
 		c.fill = GridBagConstraints.HORIZONTAL;
-		c.ipady = 20;      //make this component tall
+		c.ipady = 20;
 		c.weightx = 0.0;
 		c.gridwidth = 3;
 		c.gridx = 0;
 		c.gridy = 5;
 		this.add(bt_Join, c);
-		
 
-		this.setPreferredSize(getMinimumSize());
-	}
-	public void paintComponent(Graphics g)
-	{
-		BufferedImage img = null;
-		try 
-		{
-			img = ImageIO.read(new File("lobby_bg.jpg"));
-			g.drawImage(img, 0, 0, this.i_ScreenX, this.i_ScreenY, 0, 0, img.getWidth(), img.getHeight(), new Color(0,0,0), null);
-		} 
-		catch (Exception e) 
-		{
-			e.printStackTrace();
+		lbl_Error = new JLabel();
+		lbl_Error.setText("");
+		lbl_Error.setBackground(new Color (255,50,50));
+		lbl_Error.setOpaque(true);
+		lbl_Error.setForeground(new Color (255,255,255));
+		lbl_Error.setVisible(false);
+		c.fill = GridBagConstraints.LINE_END;
+		c.ipady = 5;
+		c.weightx = 0.0;
+		c.gridwidth = 3;
+		c.gridx = 0;
+		c.gridy = 6;
+		c.insets=new Insets(10,5,2,5);
+		this.add(lbl_Error, c);
 
-		}
+		this.setOpaque(false);
+
 	}
-	public void setScreen(int x, int y) {
-		this.i_ScreenX=x;
-		this.i_ScreenY=y;
+
+	/**sanitize the given username*/
+	public String checkUsername(String s_Username)
+	{		
+		return s_Username.replaceAll("[^A-Za-z0-9]", "");
 	}
 }

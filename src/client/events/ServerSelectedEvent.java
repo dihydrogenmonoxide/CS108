ffb909034efe9df1630989f789c2dd933a1af65d
oliver.wisler@stud.unibas.ch <oliver.wisler@stud.unibas.ch@e1f4d26a-62b9-0410-ac6e-ecfb58f24666>
2012-03-11 19:26:09 +0000

    testing
    git-svn-id: https://subversion.cs.unibas.ch/repos/fs12/cs108/team01@227 e1f4d26a-62b9-0410-ac6e-ecfb58f24666

---
+++ src/client/events/ServerSelectedEvent.java
@@ -0,0 +1,45 @@
+package client.events;
+
+import java.util.EventObject;
+
+import shared.ServerAddress;
+
+/**
+ *  
+ * @author oliver
+ *
+ */
+public class ServerSelectedEvent extends EventObject 
+{
+	/**serial.*/
+	private static final long serialVersionUID = 1L;
+	/**holds the Address of the selected Server.*/
+	private ServerAddress a;
+	/**holds the desired Username.*/
+	private String u;
+	/**Constructs the Event.
+	 * @param source Eventsource
+	 * @param server Server which is selected.
+	 * @param user desired Username.
+	 * */
+	public ServerSelectedEvent(final Object source, final ServerAddress server, final String user) 
+	{
+		super(source);
+		this.a =  server;
+		this.u = user;
+	}
+	/**Return the ServerAddress.
+	 * @return a the selected Server
+	 * */
+	public final ServerAddress getServer()
+	{
+		return a;
+	}
+	/**return the desired Username.
+	 * @return u the desired Username.
+	 * */
+	public final String getUsername()
+	{
+		return this.u;
+	}
+}

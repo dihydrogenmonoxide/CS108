bd6efbdfbebcf31a8e0eff0d7b78cf35792e5db3
user <user@MPC>
2012-03-11 13:57:57 +0100

    initial commit

---
+++ src/shared/SocketCreationException.java
@@ -0,0 +1,16 @@
+package shared;
+
+public class SocketCreationException
+extends Exception
+{
+	public SocketCreationException()
+	{
+		super();
+	}
+	
+	public SocketCreationException(String S_MSG)
+	{
+		super(S_MSG);
+	}
+
+}

b8cb2bf55080c33bd383a93cd5e4b2f0c24fa059
oliver.wisler@stud.unibas.ch <oliver.wisler@stud.unibas.ch@e1f4d26a-62b9-0410-ac6e-ecfb58f24666>
2012-03-16 23:23:40 +0000

    Switched to jdk7
    git-svn-id: https://subversion.cs.unibas.ch/repos/fs12/cs108/team01@513 e1f4d26a-62b9-0410-ac6e-ecfb58f24666

--- src/server/parser/Parser.java
+++ src/server/parser/Parser.java
@@ -1,5 +1,7 @@
 package server.parser;
 
+import java.util.UUID;
+
 import shared.Log;
 
 public class Parser 
@@ -12,9 +14,31 @@ public class Parser
 	 */
 	public String Parse(String s_MSG)
 	{
-		Log.InformationLog("Recived: \'"+s_MSG+"\'" );
+		Log.InformationLog("Received: \'"+s_MSG+"\'" );
 		
-		return "Brool story co\nline2\nline3";
+		switch(s_MSG.substring(0, 4).toUpperCase())
+		{
+		case "VAUTH":
+			if(s_MSG.length()>6)
+			{
+				String s_PlayerID = s_MSG.substring(5, s_MSG.length()-1);
+				//TODO reconnect the player
+				return s_PlayerID;
+			}
+			else
+			{
+				//TODO store the UUID in the right place
+				String uuid = UUID.randomUUID().toString();
+				
+				return uuid;
+			}
+		case "VPING":
+			return "VPONG";
+		case "CCHAT":
+			return s_MSG;
+		}
+		
+		return "not implemented yet";
 	}
 
 }

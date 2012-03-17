9b793f59c8092c523225e8b86b43c40aca51b74d
user <user@MPC>
2012-03-17 17:53:05 +0100

    Adjusted for current Server development

--- src/test/net/ServerCombination.java
+++ src/test/net/ServerCombination.java
@@ -1,9 +1,7 @@
 package test.net;
 
-import server.net.DiscoveryServer;
-import server.net.Serversocket;
-import server.parser.Parser;
-import shared.SocketCreationException;
+import server.MainServer;
+
 
 public class ServerCombination {
 
@@ -11,20 +9,8 @@ public class ServerCombination {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// create discovery Server
-		DiscoveryServer discS = new DiscoveryServer(9002);
-		Thread t = new Thread(discS);
-		t.start();
-		
-		//create socket Server
-		Parser p = new Parser();
-		try {
-			Serversocket sockS = new Serversocket(9002, p);
-			sockS.start_();
-		} catch (SocketCreationException e) {
-			e.printStackTrace();
-		}
-		
+		MainServer.startServer(9003);
+				
 	}
 
 }

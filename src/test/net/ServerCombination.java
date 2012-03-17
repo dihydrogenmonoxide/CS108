f6c530912d3ecf1b31dec1237cac3ff14b2dfb21
user <user@MPC>
2012-03-17 17:54:36 +0100

    Adjusted for new serverzzzzz

--- src/test/net/ServerCombination.java
+++ src/test/net/ServerCombination.java
@@ -3,14 +3,10 @@ package test.net;
 import server.MainServer;
 
 
-public class ServerCombination {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+public class ServerCombination
+{
+	public static void main(String[] args)
+	{
 		MainServer.startServer(9003);
-				
 	}
-
 }

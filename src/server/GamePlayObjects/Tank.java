281f651f34ce382b01401afdd41c86fbc2e64e7f
darthluzifer <lucius.bachmann@gmx.ch>
2012-03-18 13:12:28 +0100

    Implemented the move() Method for the Tank.

--- src/server/GamePlayObjects/Tank.java
+++ src/server/GamePlayObjects/Tank.java
@@ -3,7 +3,14 @@ package server.GamePlayObjects;
 import shared.Coordinates;
 import shared.User;
 
-public class Tank extends Unit implements GamePlayObject, InterAct{
+/**
+ * The Server Side Class Tank. Range is the Range in which the Tank can Attack.
+ * movingRange is the Range the Tank can move in one Phase.
+ * 
+ * @author lucius
+ * 
+ */
+public class Tank extends Unit implements GamePlayObject, InterAct {
 	private Coordinates position;
 	private int healthPoints;
 	private int range;
@@ -11,114 +18,193 @@ public class Tank extends Unit implements GamePlayObject, InterAct{
 	private Coordinates target;
 	private int movingRange;
 	private User Owner;
-	
-	public Tank(Coordinates pos, User owner){
-		this.position=pos;
-		this.healthPoints=1000;
-		this.range=10;
-		this.attackPoints=20;
+
+	public Tank(Coordinates pos, User owner) {
+		this.position = pos;
+		this.healthPoints = 1000;
+		this.range = 10;
+		this.attackPoints = 20;
 		this.build();
-		this.Owner=owner;
-		
+		this.Owner = owner;
+		this.movingRange = 50;
+
 	}
-	public void draw(){}
-	
-	public Coordinates getPos(){
+
+	public void draw() {
+	}
+
+	public Coordinates getPos() {
 		return this.position;
 	}
-	
-	//Asks the Server if the Object can be build here.
-	//If true, the Object gets build.
-	public void build(){
-		/*To Do: Ask the Server if a Tank can be build at this.position
-		 * if True, build the Object.
-		 * if false, destroi it and message to Client: Can't be build here
+
+	/**
+	 * Asks the Server if the Object can be build here. //If true, the Object
+	 * gets build.
+	 * 
+	 */
+	public void build() {
+		/*
+		 * To Do: Ask the Server if a Tank can be build at this.position if
+		 * True, build the Object. if false, destroi it and message to Client:
+		 * Can't be build here
 		 */
-		
+
 	}
-	
-	//Loads the Animation of its destruction and deletes all References
-	//that the GarbageCollector kills the Object.
-	public void destruct(){
+
+	/**
+	 * Deletes all References //that the GarbageCollector kills the Object.
+	 * 
+	 */
+	public void destruct() {
 		// To do: set all references to null
-		
-		
+
 	}
-	
-	//Returns the Health Points of the Object
-	public int getHealthPoints(){
-		
+
+	/**
+	 * Returns the Health Points of the Object
+	 * 
+	 */
+	public int getHealthPoints() {
+
 		return this.healthPoints;
 	}
-	
-	//Hits this Object with Damage.
-	public void damage(int damPoints){
-		this.healthPoints-=damPoints;
-		if(this.getHealthPoints()<=0)
-		{
+
+	/**
+	 * Hits this Object with Damage.
+	 * 
+	 */
+	public void damage(int damPoints) {
+		this.healthPoints -= damPoints;
+		if (this.getHealthPoints() <= 0) {
 			this.destruct();
 		}
-		
+
 	}
-	
-	//get the Range of the Tank
+
+	/**
+	 * get the Range of the Tank
+	 * 
+	 */
 	public int getRange() {
-		
+
 		return this.range;
 	}
-	
-	//Attack the target
-	@Override
+
+	/**
+	 * Attack the target
+	 * 
+	 */
+
 	public void attack(GamePlayObject target) {
-		if(this.isAttackableObject(target) && this.isInRange(target))
-		{
+		if (this.isAttackableObject(target) && this.isInRange(target)) {
 			target.damage(this.getAttackPoints());
-			
+
 		}
-		
+
 	}
-	
-	//get the Tank's AttackPoints
+
+	/**
+	 * get the Tank's AttackPoints
+	 * 
+	 * @return
+	 */
 	private int getAttackPoints() {
-		
+
 		return this.attackPoints;
 	}
-	
-	//is the target in the Tank's Range
-	@Override
+
+	/**
+	 * is the target in the Tank's attack Range
+	 * 
+	 */
+
 	public boolean isInRange(GamePlayObject target) {
-		if(this.range>=this.position.getDistance(target.getPos()))return true;
+		if (this.range >= this.position.getDistance(target.getPos()))
+			return true;
 		return false;
 	}
-	
-	//is the target Attackable by a Tank
-	@Override
-	public boolean isAttackableObject(GamePlayObject target) {
-		
-		return true;//this is for Testing, that Tank1 can Attack Tank2 in the Test.
-					//Remark that Tank3 is out of Range!!!
-		
-		//The Code below works too.
-		//if( target instanceof Building && this.getOwner()!=target.getOwner())return true;
-		//return false;
-	}
-	
-	//get the current Target of the Tank.
-	public Coordinates getTarget(){
-		
-		return this.target;
-		
-	}
-	
-	
-	public void setTarget(Coordinates target){
-		
-			this.target=target;
-	}
-	
 
-	public User getOwner(){
+	/**
+	 * is the target Attackable by a Tank (Planes are not attackable by a
+	 * Tank..., Objects of the same Owner should not attack each other....
+	 */
+
+	public boolean isAttackableObject(GamePlayObject target) {
+
+		if ((target instanceof Building || target instanceof Tank)
+				&& this.getOwner() != target.getOwner())
+			return true;
+		return false;
+	}
+
+	/**
+	 * get the current Target of the Tank.
+	 * 
+	 * @return Coordinates of the Target to Move
+	 */
+	public Coordinates getTarget() {
+
+		return this.target;
+
+	}
+
+	/**
+	 * Set Coordinates as Moving Target
+	 * 
+	 * @param target
+	 */
+	public void setTarget(Coordinates target) {
+
+		this.target = target;
+	}
+
+	/**
+	 * returns the Owner of this Object.
+	 */
+	public User getOwner() {
 		return this.Owner;
-		
+
+	}
+
+	/**
+	 * Moves the Object to its Target. Because of DivisionbynullException and
+	 * Overflowing he moves as long as the X or Y coordinates are not Reached
+	 * with a Stepsize of 10 for X, and an Y Stepsize of deltaY/deltaX*10
+	 * 
+	 */
+	public void move() {
+		int direction = 1;
+		int alreadyMovedSquared = 0;
+		double deltaY = this.target.getY() - this.position.getY();
+		double deltaX = this.target.getX() - this.position.getX();
+
+		if (deltaY < 0 && deltaX < 0)
+			direction = -1;
+		while (target != null
+				&& !(this.target.getX() == this.position.getX() && this.target
+						.getY() == this.position.getY())
+				&& alreadyMovedSquared <= this.movingRange * this.movingRange) {
+
+			if (this.target.getX() == this.position.getX()) {
+				this.position.moveY(1);
+				alreadyMovedSquared++;
+
+			} else if (this.target.getY() == this.position.getY()) {
+				this.position.moveX(1);
+				alreadyMovedSquared++;
+
+			} else {
+
+				this.position.moveX(10*direction);
+
+				int mY = (int) Math.round(deltaY / deltaX * direction * 10);
+
+				this.position.moveY(mY);
+
+				alreadyMovedSquared += 100 + mY;
+			}
+
+		}
+
 	}
 }
--- src/server/GamePlayObjects/Test.java
+++ src/server/GamePlayObjects/Test.java
@@ -7,7 +7,7 @@ public class Test {
 	public static void main(String[] args){
 		Tank Tank1= new Tank(new Coordinates(5,5), new User("Lucius"));
 		Tank Tank2= new Tank(new Coordinates(10,5), new User("Frank"));
-		Tank Tank3= new Tank(new Coordinates(100,5), new User("Fabio"));
+		Tank Tank3= new Tank(new Coordinates(1000,1000), new User("Fabio"));
 		int counter =0;
 		while(counter < 100 &&(Tank1.getHealthPoints()>0 && Tank1.getHealthPoints()>0 && Tank1.getHealthPoints()>0))
 		{
@@ -25,6 +25,10 @@ public class Test {
 			System.out.println("Tank 3 has "+Tank3.getHealthPoints()+" HealthPoints");
 			counter++;
 		}
+		
+		Tank3.setTarget(new Coordinates(950,950));
+		Tank3.move();
+		System.out.println(Tank3.getPos().getX()+" / "+Tank3.getPos().getY());
 		
 		
 		

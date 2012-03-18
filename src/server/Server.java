5eaefccc79cf950ef2f446617db7fb879d8b9ed3
user <user@MPC>
2012-03-18 17:00:04 +0100

    fixes, fixes and fixes along with further protocol implementation

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,6 +1,7 @@
 package server;
 
 import java.util.Collections;
+import java.util.Formatter;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Vector;
@@ -13,14 +14,16 @@ implements Comparable<Server>
 	private List<Player> l_locked;
 	private String s_servername;
 	private int i_ServerID;
-	private static int i_NumServers = 0;
 	
-	public Server(String s_Servername)
+	public Server(String s_Servername, int i_ID)
+	
 	{
 		// TODO implement the whole server a user can start when he's in the lobby
 		s_servername = s_Servername;
 		l_locked = Collections.unmodifiableList(l_players);
-		i_ServerID = i_NumServers++;
+		i_ServerID = i_ID+200;
+		MainServer.getServerManager().addServer(this);
+		MainServer.getPlayerManager().broadcastMessage_everyone("GGAME "+this.getID()+" "+this.getPlayerAmount()+"  "+this.getServername());
 	}
 	
 	
@@ -44,8 +47,21 @@ implements Comparable<Server>
 	 * @param p_Player the player
 	 */
 	public void addPlayer(Player p_Player)
-	{
+	{	
+		p_Player.setServer(this);
 		process(p_Player, true);
+		MainServer.getPlayerManager().broadcastMessage_everyone("GJOIN "+this.i_ServerID+" "+p_Player.getID()+" "+p_Player.getNick());
+		MainServer.getPlayerManager().broadcastMessage_everyone("LQUIT "+p_Player.getID());
+		MainServer.getPlayerManager().broadcastMessage_everyone("GGAME "+this.getID()+" "+this.getPlayerAmount()+"  "+this.getServername());
+	}
+	
+	/**
+	 * Returns the amount of players connected to this Server/Game
+	 * @return the amount of players
+	 */
+	public int getPlayerAmount()
+	{
+		return this.l_locked.size();
 	}
 	
 	
@@ -55,7 +71,15 @@ implements Comparable<Server>
 	 */
 	public void removePlayer(Player p_player)
 	{
+		p_player.setServer(null);
 		process(p_player, false);
+		MainServer.getPlayerManager().broadcastMessage_everyone("GQUIT "+this.i_ServerID+" "+p_player.getID()+" "+p_player.getNick());
+		MainServer.getPlayerManager().broadcastMessage_everyone("LJOIN "+p_player.getID());
+		if(this.getPlayerAmount() == 0)
+		{
+			MainServer.getServerManager().removeServer(this);
+		}			
+			MainServer.getPlayerManager().broadcastMessage_everyone("GGAME "+this.getID()+" "+this.getPlayerAmount()+"  "+this.getServername());
 	}
 	
 	private synchronized void process(Player p_player, boolean b_add)
@@ -73,6 +97,8 @@ implements Comparable<Server>
 				Player p = i_players.next();
 				if(p == p_player)
 				{
+					i_players.remove();
+					//TODO close the server if it was the last guy and broadcast that
 					MainServer.printInformation("Removed "+p.getNick()+" from "+this.s_servername);					
 				}
 			}
@@ -88,4 +114,13 @@ implements Comparable<Server>
 		return this.s_servername;
 	}
 
+
+	/**
+	 * returns the serverID
+	 * @return the serverID
+	 */
+	public int getID()
+	{
+		return this.i_ServerID;
+	}
 }

6b40a600ffd86724fda12b2fe7a239d4d9f3c74c
dihydrogenmonoxide <hilfecenter@gmail.com>
2012-03-27 13:21:57 +0200

    initial commit

---
+++ src/server/exceptions/GameObjectBuildException.java
@@ -0,0 +1,16 @@
+package server.exceptions;
+
+public class GameObjectBuildException 
+extends Exception
+{
+
+	public GameObjectBuildException()
+	{
+		super();
+	}
+	
+	public GameObjectBuildException(String s)
+	{
+		super(s);
+	}
+}

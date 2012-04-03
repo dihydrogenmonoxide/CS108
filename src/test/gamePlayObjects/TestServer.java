e12547a32c56761ff19f31f0ff1d672181e1db4c
dihydrogenmonoxide <hilfecenter@gmail.com>
2012-04-03 12:46:50 +0200

    Added a server with fakeplayers

---
+++ src/test/gamePlayObjects/TestServer.java
@@ -0,0 +1,33 @@
+package test.gamePlayObjects;
+
+import server.MainServer;
+import server.Server;
+import server.players.Player;
+
+public class TestServer 
+{
+	private TestServer()
+	{
+		
+	}
+	public static void main(String a[])
+	{
+		startTestServer();
+	}
+	
+	public static void startTestServer()
+	{
+		MainServer.startServer(9009);
+		for(int i = 1; i != 6; i++)
+		{
+			Player p = new Player("derp", null, i+100);
+		}
+		
+		Server s = new Server("hurp", 201);
+		for(Player p : MainServer.getPlayerManager().getPlayers())
+		{
+			s.addPlayer(p);
+		}
+	}
+
+}

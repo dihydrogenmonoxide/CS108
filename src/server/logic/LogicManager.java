9ff7a1b9f2db17dcc03fbe5a442356caa27b9da1
user <user@MPC>
2012-04-07 21:10:30 +0200

    LogicManager should be fully implemented now (untested tho)

--- src/server/logic/LogicManager.java
+++ src/server/logic/LogicManager.java
@@ -1,7 +1,22 @@
 package server.logic;
 
+import server.MainServer;
 import server.Server;
+import server.GamePlayObjects.ATT;
+import server.GamePlayObjects.Bomber;
+import server.GamePlayObjects.Flak;
+import server.GamePlayObjects.GamePlayObject;
+import server.GamePlayObjects.GamePlayObjectManager;
+import server.GamePlayObjects.Jet;
+import server.GamePlayObjects.Reproductioncenter;
+import server.GamePlayObjects.Tank;
+import server.exceptions.GameObjectBuildException;
+import server.exceptions.PlayerNotFoundException;
+import server.parser.Parser;
+import server.players.Player;
+import shared.Log;
 import shared.Protocol;
+import shared.game.Coordinates;
 
 
 
@@ -9,8 +24,14 @@ import shared.Protocol;
 public class LogicManager 
 implements Runnable
 {
+	private final static int buildTimeInSeconds = 60;
+	private final static int animationTimeInSeconds = 10;
 	private Server server;
 	private Thread thread = new Thread(this);
+	private int voteCount = 0;	
+	
+	private boolean isInBuildPhase = false;
+
 	
 	public LogicManager(Server server)
 	{
@@ -20,14 +41,219 @@ implements Runnable
 	@Override
 	public void run() 
 	{
-		//TODO Implement
-		//TODO implement protocol to notify the clients that the game began
+		while(server.isGameRunning())
+		{
+			buildPhase();
+			server.getObjectManager().round();
+			updateClients();
+			animationPhase();
+			//TODO detect game ending when one player won
+		}
 	}
 	
+	private void updateClients()
+	{
+		for(Player p : server.getPlayers())
+		{
+			resendEverything(p);
+		}
+		
+	}
+
+	/**
+	 * Starts the game. This is called automatically by the {@link Server}
+	 */
 	public void startGame()
 	{
+		for(Player p : server.getPlayers())
+		{
+			p.sendData(Protocol.GAME_BEGIN.str()+server.getID()+" "+p.getFieldID());
+		}
+		
 		thread.start();
-		//FIXME please send the GameId with it, thx Oli
-		server.broadcastMessage(Protocol.GAME_BEGIN.str());
+	}
+	
+	private void animationPhase()
+	{
+		server.broadcastMessage(Protocol.GAME_ANIMATION_PHASE.str()+animationTimeInSeconds);
+		try
+		{
+			Thread.sleep(animationTimeInSeconds*1000);
+		}
+		catch(InterruptedException e)
+		{
+			Log.WarningLog("The animationphase was interruped, this should not be done!");
+		}
+		finally
+		{
+			server.broadcastMessage(Protocol.GAME_ANIMATION_PHASE.str()+0);
+		}
+	}
+
+	
+	private void buildPhase()
+	{
+		voteCount = 0;
+		for(Player p : server.getPlayers())
+		{
+			p.endBuildPhase();
+		}
+		server.broadcastMessage(Protocol.GAME_BUILD_PHASE.str()+buildTimeInSeconds);
+		isInBuildPhase = true;
+		try
+		{
+			Thread.sleep(buildTimeInSeconds*1000+1000);
+		}
+		catch (InterruptedException e)
+		{
+			Log.DebugLog("All Players finished building before tiem ran out");
+		}
+		isInBuildPhase = false;
+		server.broadcastMessage(Protocol.GAME_BUILD_PHASE.str()+0);
+	}
+	
+	public void resendEverything(Player player)
+	{
+		for(GamePlayObject o :server.getObjectManager().getObjectList())
+		{
+			player.sendData(o.toProtocolString());
+		}
+		player.sendData(Protocol.GAME_MONEY.str()+player.getMoney());
+		player.sendData(Protocol.GAME_POPULATION.str()+player.getPopulation());
+	}
+
+	public synchronized void finishedBuilding(Player player)
+	{
+		if(isInBuildPhase && !player.finishedBuilding())
+		{
+			voteCount++;
+			player.finishedBuilding(true);
+			if(voteCount >= server.getPlayerAmount())
+			{
+				thread.interrupt();
+				Log.InformationLog("Everyone has finished b4 time ran out");
+			}
+		}
+		else
+		{
+			player.sendData(Protocol.CON_ERROR.str()+"You already stated, that you're finished!");
+		}
+	}
+
+	public void buildObject(String s_MSG, Player player)
+	{
+		if(server.isPaused())
+		{
+			player.sendData(Protocol.CON_ERROR.str()+"The Server is paused, please wait for it to resume.");
+			return;
+		}
+		if(!isInBuildPhase)
+		{
+			player.sendData(Protocol.CON_ERROR.str()+"You can't build outside a build phase.");
+			return;
+		}
+		
+		String[] s = s_MSG.split("\\s+");
+		if(s.length == 3)
+		{
+			int x = Integer.parseInt(s[1]);
+			int y = Integer.parseInt(s[2]);
+			synchronized (player)
+			{
+				try
+				{
+					GamePlayObject o;
+					
+					switch(Protocol.fromString(s[0]))
+					{
+					case OBJECT_BOMBER:
+						o = new Bomber(new Coordinates(x, y), player, server.getObjectManager());
+						break;
+					case OBJECT_FIGHTER_JET:
+						o = new Jet(new Coordinates(x, y), player, server.getObjectManager());
+						break;
+					case OBJECT_REPRODUCTION_CENTER:
+						o = new Reproductioncenter(new Coordinates(x, y), player, server.getObjectManager());
+						break;
+					case OBJECT_STATIONARY_ANTI_AIR:
+						o = new Flak(new Coordinates(x, y), player, server.getObjectManager());
+						break;
+					case OBJECT_STATIONARY_ANTI_TANK:
+						o = new ATT(new Coordinates(x, y), player, server.getObjectManager());
+						break;
+					case OBJECT_TANK:
+						o = new Tank(new Coordinates(x, y), player, server.getObjectManager());
+						break;	
+					default:
+						Log.ErrorLog("Couldn't build the object - \'"+s_MSG+"\' isn't in a valid format");
+						return;
+					}
+					
+					player.sendData(o.toProtocolString());
+					player.addObject(o);
+				}
+				catch(GameObjectBuildException e)
+				{
+					Log.InformationLog(player.getNick()+": "+e.getMessage());
+					player.sendData(Protocol.CON_ERROR.str()+e.getMessage());
+					return;
+				}
+				
+			}
+		}
+
+		Log.ErrorLog("Couldn't build the object - \'"+s_MSG+"\' isn't in a valid format");
+	}
+	
+	/**
+	 * updates the given object
+	 * @param s_MSG the options received from the {@link Parser}
+	 * @param player the {@link Player} requesting the update
+	 */
+
+	public void updateObject(String s_MSG, Player player)
+	{
+		if(server.isPaused())
+		{
+			player.sendData(Protocol.CON_ERROR.str()+"The Server is paused, please wait for it to resume.");
+			return;
+		}
+		if(!isInBuildPhase)
+		{
+			player.sendData(Protocol.CON_ERROR.str()+"You can't build outside a build phase.");
+			return;
+		}
+		
+		String[] s = s_MSG.split("\\s+");
+		if(s.length == 4)
+		{
+			int x = Integer.parseInt(s[1]);
+			int y = Integer.parseInt(s[2]);
+			int id = Integer.parseInt(s[3]);
+			synchronized (player)
+			{
+				GamePlayObject o = server.getObjectManager().getObjectById(id);
+				if(o != null)
+				{
+					if(o.getOwner() == player)
+					{
+						o.setTarget(new Coordinates(x, y));
+						//TODO evaluate whether to send the new position or not
+					}
+					else
+					{
+						player.sendData(Protocol.CON_ERROR.str()+"trying to move another players stuff... so silly!");
+						MainServer.printInformation(player.getNick()+" just attempted to move "+o.getOwner().getNick()+"\'s object!");
+					}
+				}
+				else
+				{
+					player.sendData(Protocol.CON_ERROR.str()+"Can't find an object with the ID "+id);
+					Log.WarningLog("no object with the ID "+id+" found - can't move it therefore");
+				}
+				return;
+			}
+		}
+		Log.ErrorLog("Couldn't move the object - \'"+s_MSG+"\' isn't in a valid format");
 	}
 }

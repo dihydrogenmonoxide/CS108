5a89257e32dee75dd7f651e18f766c7062e14e63
fabio sulser <fabio.sulser@gmail.com>
2012-04-12 20:01:04 +0200

    Lines are drawable

--- src/client/game/Line.java
+++ src/client/game/Line.java
@@ -2,38 +2,87 @@ package client.game;
 
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
-class GrafikPanel extends JPanel{
-    private int ystart,xstart;
-    private int yend, xend;
-    public GrafikPanel(){
-        ystart= 40;
-        xstart= 100;
-        yend =700;
-        xend=700;
+public class Line extends JPanel implements ActionListener{
+	Timer timer;
+
+	
+	private double xstart=100;
+	private int ystart=100;
+	private int xend=200;
+	private int yend=110;
+	final double xdif=xend-xstart;
+	double ydif=yend-ystart;
+	public Line() {
+		timer = new Timer(1000, this);
+	    timer.start();
     }
-    public void paintComponent(Graphics g){
-    	g.setColor(Color.red);
-        g.drawLine(100,40,xstart,ystart);
-    }
-    public void setI(){
-        if (xend>xstart&&yend>ystart){
-        	xstart++;
-        	ystart++;
-        }
-    }
-}
-class Kurve extends Thread {
-    private GrafikPanel line;
-    public Kurve(GrafikPanel line) {
-        this.line = line;
-    }
-    public void run() {
-        while(true){
-            try {sleep(1);} catch (InterruptedException e) {}
-            line.repaint();
-            line.setI();
-        }
-    }
+
+	public void paint(Graphics g) {
+
+		Graphics2D g2d = (Graphics2D) g;
+	    g2d.setStroke(new BasicStroke(3));    
+	    g2d.setColor(Color.red);
+		g2d.drawLine((int)xstart, ystart, (int) xstart, ystart);
+	   
+	}
+
+	public void actionPerformed(ActionEvent e) {
+
+		if(xdif==0){
+			if(ystart<=yend){
+				ystart++;
+				  
+			}
+			if(ystart>=yend){
+				ystart--;
+				}
+			}
+
+		else{
+			if(ydif==0){
+				if(xstart<=xend){
+					xstart++;
+				}
+				if(xstart>=xend){
+					  xstart--;
+				}
+			}
+			else{
+				double fact=xdif/ydif;
+				if(xdif<=0&&ydif<=0){
+					if(xstart>=xend){
+						xstart-=fact;
+						ystart--;
+					}
+				}
+		  
+				if(xdif<=0&&ydif>=0){
+					if(xstart>=xend){
+						xstart+=fact;
+						ystart++;
+					}
+				}
+		  
+				if(xdif>=0&&ydif<=0){
+					if(xstart<=xend){
+						xstart-=fact;
+						ystart--;
+					}
+				}
+		  
+				if(xdif>=0&&ydif>=0){
+					if(xstart<=xend){
+						xstart+=fact*02;
+						ystart++;
+					}
+				}
+			}
+		}
+		repaint();
+	}
 }

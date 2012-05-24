50bdb4b7b141c43d027596ddeec9fef9554c92de
Fabio Sulser <fabio.sulser@gmail.com>
2012-05-24 09:41:11 +0200

    Player inf. in 4 TextFields

--- src/client/game/field/PlayerInfo.java
+++ src/client/game/field/PlayerInfo.java
@@ -18,6 +18,8 @@ public class PlayerInfo extends JPanel {
 	
 	private JTextArea moneyPanel;
 	private JTextArea populationPanel;
+	private JTextArea populationPan;
+	private JTextArea moneyPan;
 	
 	public PlayerInfo(){
 		this.setLayout(new GridBagLayout());
@@ -30,24 +32,40 @@ public class PlayerInfo extends JPanel {
 		moneyPanel.setFont(f);
 		moneyPanel.setOpaque(false);
 		moneyPanel.setForeground(new Color(150,150,150));
-		moneyPanel.setText("Kontostand: "+"\n"+ Long.toString(RunningGame.getMoney()));
+		moneyPanel.setText("Kontostand:");
 		moneyPanel.setEditable(false);
         moneyPanel.setWrapStyleWord(true);
-		c.gridx=7;
-		c.gridy=2;
+		c.gridy=0;
 		this.add(moneyPanel,c);
+		
+		moneyPan = new JTextArea();
+		moneyPan.setFont(f);
+		moneyPan.setForeground(new Color(150,150,150));
+		moneyPan.setOpaque(false);
+		moneyPan.setText(Long.toString(RunningGame.getMoney()));
+		moneyPan.setEditable(false);
+		c.gridy=1;
+		this.add(moneyPan,c);
 	
 		
 		populationPanel = new JTextArea();
 		populationPanel.setFont(f);
 		populationPanel.setOpaque(false);
 		populationPanel.setForeground(new Color(150,150,150));
-		populationPanel.setText("Population: "+"\n"+ RunningGame.getPopulation());
+		populationPanel.setText("Population:");
 		populationPanel.setEditable(false);
 		populationPanel.setWrapStyleWord(true);
-		c.gridx=7;
-		c.gridy=3;
+		c.gridy=2;
 		this.add(populationPanel,c);
+		
+		populationPan = new JTextArea();
+		populationPan.setFont(f);
+		populationPan.setOpaque(false);
+		populationPan.setForeground(new Color(150,150,150));
+		populationPan.setText(Long.toString(RunningGame.getPopulation()));
+		populationPan.setEditable(false);
+		c.gridy=3;
+		this.add(populationPan,c);
 		
 		this.setOpaque(false);
 		Update tcMin = new Update();
@@ -70,11 +88,11 @@ public class PlayerInfo extends JPanel {
 		public void actionPerformed(ActionEvent f){
             money=  RunningGame.getMoney();
 
-			moneyPanel.setText("Kontostand:" + longToString(money));
+			moneyPan.setText(longToString(money));
 
 			population= RunningGame.getPopulation();
 			
-			populationPanel.setText("Population:"+longToString(population));
+			populationPan.setText(longToString(population));
 			
 		}
 
